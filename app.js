151279ccb3e61001efc9e6c42bbc6a1bee058e6f
natalypazm <pazmoralesnataly@gmail.com>
2020-11-06 19:46:21 -0500

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
-
 const searchRoute = require("./services/search");
 
+app.use(cors());
 app.use("/", searchRoute);
 
 app.listen(4300);
